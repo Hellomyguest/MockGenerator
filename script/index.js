0f633eeefd8ed45e1b40bac737854f79e10945b8
AIllarionov <hellomyguest@yandex.ru>
2022-11-27 15:35:52 +0300

    added generateUUID function

--- script/index.js
+++ script/index.js
@@ -3,6 +3,15 @@ function Randomizer(min, max) {
   return Math.floor(rand);
 }
 
+function generateUUID() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
+
 function randomDate(
   start = new Date("2021, 01, 01"),
   end = new Date(),
@@ -16,16 +25,16 @@ function randomDate(
 function getOrderNumber() {
   let order = "";
   for (let i = 0; i < 6; i++) {
-    order += Randomizer(0,9);
+    order += Randomizer(0, 9);
   }
   return order;
 }
 
 function getOrder(length) {
   let arr = [];
-  let arrCheck = []
+  let arrCheck = [];
   for (let i = 0; i < length; i++) {
-    let rand = Randomizer(0, 166)
+    let rand = Randomizer(0, 166);
     while (arrCheck.includes(rand)) {
       rand = Randomizer(0, 166);
     }
@@ -36,10 +45,10 @@ function getOrder(length) {
 }
 
 function getLoyality(sum) {
-  if (sum > 250000) return 'legend';
-  if (sum > 100000) return 'professional';
-  if (sum > 50000) return 'amateur';
-  return 'newbie'
+  if (sum > 250000) return "legend";
+  if (sum > 100000) return "professional";
+  if (sum > 50000) return "amateur";
+  return "newbie";
 }
 
 function genOrders(number) {
@@ -47,35 +56,42 @@ function genOrders(number) {
   for (let i = 0; i < number; i++) {
     let amount = Randomizer(1, 5);
     let order = getOrder(amount);
-    let sum = order.reduce((sum, item) => sum += item.price, 0);
+    let sum = order.reduce((sum, item) => (sum += item.price), 0);
+    let id = generateUUID();
+    while (arr.some((item) => item.id === id)) {
+      console.log("совпадение");
+      id = generateUUID();
+    }
 
     arr.push({
-      id: IDS[i],
-      customer: `${SURNAMES[Randomizer(0, 196)]} ${
-        NAMES[Randomizer(0, 196)]
-      } ${PATRONYMICS[Randomizer(0, 199)]}`,
+      id: id,
+      customer: `${SURNAMES[Randomizer(0, 196)]} ${NAMES[Randomizer(0, 196)]} ${
+        PATRONYMICS[Randomizer(0, 199)]
+      }`,
       date: randomDate(),
-      status: STATUS[Randomizer(0,5)],
+      status: STATUS[Randomizer(0, 5)],
       amount: amount,
       orderNumber: getOrderNumber(),
       order: order,
       sum: sum,
-      loyality: getLoyality(sum)
+      loyality: getLoyality(sum),
     });
   }
   return arr;
 }
 
-let input = document.getElementById('length');
-let textarea = document.getElementById('mocks');
+let input = document.getElementById("length");
+let textarea = document.getElementById("mocks");
 
-submit.onclick = function() {
+submit.onclick = function () {
   let length = input.value;
-  textarea.innerHTML=JSON.stringify(length? genOrders(length) : 'Введите кол-во записей');
+  textarea.innerHTML = JSON.stringify(
+    length ? genOrders(length) : "Введите кол-во записей"
+  );
   clear.style.display = "flex";
 };
 
-clear.onclick = function() {
-  textarea.innerHTML = '';
+clear.onclick = function () {
+  textarea.innerHTML = "";
   clear.style.display = "none";
-}
+};
